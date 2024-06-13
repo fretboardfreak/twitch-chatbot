0c68b7d43f51b96fc8e21c47f194a1d0899caa72
Curtis Sand <curtissand@gmail.com>
2024-06-12 20:21:42 -0700

    lint.sh: clean up linters and exit if pycodestyle has errors

--- lint.sh
+++ lint.sh
@@ -3,14 +3,13 @@
 LINE_LENGTH=120
 SEP="----------"
 
-echo "$SEP pylint $SEP"
-pylint --max-line-length=$LINE_LENGTH --disable W1203 chatbot/
-echo -e "$SEP end pylint $SEP\n"
-
 echo "$SEP pycodestyle $SEP"
 pycodestyle --max-line-length=$LINE_LENGTH chatbot/
+if [[ $? -ne 0 ]]; then
+    exit
+fi
 echo -e "$SEP end pycodestyle $SEP\n"
 
-# echo "$SEP pydocstyle $SEP"
-# pydocstyle chatbot/
-# echo -e "$SEP end pydocstyle $SEP\n"
+echo "$SEP pylint $SEP"
+pylint --max-line-length=$LINE_LENGTH --disable W1203,R0902 chatbot/
+echo -e "$SEP end pylint $SEP\n"
