01d1de240890034cdc3eb44bf851eb2850de35db
Curtis Sand <curtissand@gmail.com>
2024-06-19 09:35:42 -0700

    remove unneeded run bash script

--- run_mc_wordgame.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python twitch_chatbot.py --verbose \
-    --wordgame "I'm thinking of the name of an item that you can collect in Minecraft." \
-    --wordlist minecraft_1.20_item_list.yml \
-    --moderator fretboardfreak \
-    ${@}
