b834628e2464eba4fb79e1a33d655f3954a8a47e
Curtis Sand <curtissand@gmail.com>
2025-01-02 22:46:47 -0800

    wordgame: implement the UI portion of the rewrite

--- src/tests/test_wordgame.py
+++ src/tests/test_wordgame.py
@@ -19,14 +19,18 @@ import pytest
 import chatbot.wordgame as wordgame
 
 
+wordgame.DEBUG = True
+
+
 @pytest.fixture
-def uncache_wordgame_data():
-    wordgame.Wordgame._data = None
+def cleanup():
     yield
+    wordgame.DEBUG = True
+    wordgame.Wordgame.clear_caches()
+
+
+def test_load_data(cleanup):
     wordgame.Wordgame._data = None
-
-
-def test_wordgame_load_data(uncache_wordgame_data):
     assert wordgame.Wordgame._data is None
     wordgame.Wordgame.load_word_data()
     assert wordgame.Wordgame._data is not None
@@ -38,10 +42,93 @@ def test_wordgame_load_data(uncache_wordgame_data):
     assert id1 == id2
 
 
-def test_wordgame_categories():
+@pytest.mark.parametrize('debug', [(False), (True)])
+def test_categories(debug, cleanup):
     wordgame.Wordgame.load_word_data()
     assert "test" in wordgame.Wordgame._data.keys()
+
+    wordgame.DEBUG = debug
     categories = wordgame.Wordgame.word_categories()
     assert isinstance(categories, list)
     assert isinstance(categories[0], str)
-    assert "test" not in categories
+    if debug:
+        assert "test" in categories
+    else:
+        assert "test" not in categories
+
+
+@pytest.mark.parametrize('category', [(None), ('test')])
+def test_choose_word(category, cleanup):
+    instance = wordgame.Wordgame()
+    assert instance.secret_word is None and instance.secret_word_category is None
+
+    with instance.lock:
+        success = instance.choose_word(category)
+
+    assert success
+    assert isinstance(instance.secret_word, str) and instance.secret_word
+    if category:
+        assert (isinstance(instance.secret_word_category, str)
+                and instance.secret_word_category == category)
+
+    else:
+        assert (isinstance(instance.secret_word_category, str)
+                and instance.secret_word_category)
+
+def test_clear_caches(cleanup):
+    wordgame.Wordgame.word_categories()
+    assert wordgame.Wordgame._data is not None and wordgame.Wordgame._categories is not None
+
+    wordgame.Wordgame.clear_caches()
+    assert wordgame.Wordgame._data is None and wordgame.Wordgame._categories is None
+
+@pytest.mark.parametrize('secret_word,guesses,expected_censored',
+                         [('foo', {}, '_ _ _'),
+                          ('Foo', {'f'}, 'f _ _'),
+                          ('foo', {'o'}, '_ o o'),
+                          ('tetheh', {'th'}, '_ _ t h _ _'),
+                          ('foo bar baz', {'o', 'a'}, '_ o o - _ a _ - _ a _'),
+                          ('Pun*ct#ua(tioN', {}, '_ _ _ _ _ _ _ _ _ _ _'),
+                          ])
+def test_build_censored_word(secret_word, guesses, expected_censored, cleanup):
+    instance = wordgame.Wordgame()
+
+    instance.secret_word = secret_word
+    instance.guesses = guesses
+
+    with instance.lock:
+        instance.build_censored_word()
+
+    assert instance.censored_word == expected_censored
+
+
+@pytest.mark.parametrize('secret_word,guesses,expected_censored,is_new,is_good',
+                         [('foobar', ['o'], '_ o o _ _ _', True, True),
+                          ('foobar', ['o', 'o'], '_ o o _ _ _', False, True),
+                          ('foobar', ['x'], '_ _ _ _ _ _', True, False),
+                          ('foo bar', [' '], '_ _ _ - _ _ _', True, True),
+                          ])
+def test_guess(secret_word, guesses, expected_censored, is_new, is_good, cleanup):
+    instance = wordgame.Wordgame()
+
+    instance.secret_word = secret_word
+
+    with instance.lock:
+        instance.build_censored_word()
+
+        for guess in guesses:
+            instance.guess(guess)
+
+    assert instance.censored_word == expected_censored
+
+
+def test_endgame(cleanup):
+    instance = wordgame.Wordgame()
+    instance.secret_word = 'secret word'
+
+    with instance.lock:
+        instance.build_censored_word()
+        instance.guess('secret')
+
+        with pytest.raises(wordgame.EndgameException):
+            instance.guess('word')
